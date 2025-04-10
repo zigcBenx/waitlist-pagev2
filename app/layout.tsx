b7bb7c5aca4a1f87af522bd614f686cd4463bf16
Ziga Benko <zigac.benko@gmail.com>
2025-04-10 12:28:09 +0200

    Fix meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'From Code to Business',
+  description: 'A guide to starting a business as a full time developer',
+  keywords: ['From Code to Business', 'Starting a business', 'Full time developer'],
 }
 
 export default function RootLayout({
